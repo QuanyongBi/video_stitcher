1dfc985750fb9af49c33a8a1d66bf894a4aadd7f
Jason <quanyong@usc.edu>
2025-04-23 14:58:16 -0700

    some desperate brainstorm

--- src/feature_matching.py
+++ src/feature_matching.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
-def detect_and_match_features(img1, img2):
+def detect_and_match_features(frame_cur, frame_prev, feature_num):
+    img1 = frame_cur
+    img2 = frame_prev
     # Convert to grayscale
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    # gray1 = img1
     gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     
-    orb = cv2.ORB_create(nfeatures=2500)
+    orb = cv2.ORB_create(nfeatures=feature_num)
     
     kp1, desc1 = orb.detectAndCompute(gray1, None)
     kp2, desc2 = orb.detectAndCompute(gray2, None)
@@ -22,14 +25,16 @@ def detect_and_match_features(img1, img2):
     # Apply ratio test
     good_matches = []
     for m,n in matches:
-        if m.distance < 0.75 * n.distance:
+        if m.distance < 0.5 * n.distance:
             good_matches.append(m)
-    
+
     print(f"Found {len(good_matches)} good matches")
     return kp1, desc1, kp2, desc2, good_matches
 
 
-def find_transformation(kp1, kp2, matches, method='homography'):
+def find_transformation(kp_cur, kp_prev, matches, method='homography'):
+    kp1 = kp_cur
+    kp2 = kp_prev
     if len(matches) < 4:
         print("Not enough matches to find homography")
         return None, None
@@ -37,6 +42,6 @@ def find_transformation(kp1, kp2, matches, method='homography'):
     src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
     dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
     
-    H, status = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    H, status = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 3.0)
     
     return H, status
--- src/test.py
+++ src/test.py
@@ -1,21 +1,36 @@
 from video_utils import compress_video, extract_and_save_frames
 from output_visualize import visualize_output
-from video_stitcher import stitch_images
+from video_stitcher import stitch_images_linear, stitch_images_stack, stitch_images_divide_conquer
+from color_alignment import correct_all_frames
+import cv2
 
 def main():
     # Extract frames from video
-    video_path = "data/video_data/video1/real_001.mp4"
+    video_path = "data/video_data/video1/real_004.mp4"
     compress_video(video_path,
-               "data/compressed/hd_halfres.mp4",
+               output_path="data/compressed/hd_halfres.mp4",
                scale=0.5,
                codec='mp4v')
-    high_res_frames = extract_and_save_frames("data/video_data/video1/real_001.mp4", "data/extracted_frames", 16)
-    frames = extract_and_save_frames("data/compressed/hd_halfres.mp4", "data/extracted_frames", 16)
+    # high_res_frames = extract_and_save_frames("data/video_data/custom/bedroom.MOV", "data/extracted_frames", 16)
+    frames = extract_and_save_frames("data/compressed/hd_halfres.mp4", "data/extracted_frames", 2)
+    corrected = correct_all_frames(frames)
     
     # Visualizing the output
-    output = stitch_images(frames, high_res_frames, False)
+    # output = stitch_images_linear(corrected, False)
+    # if output is not None:
+    #     visualize_output(frames, output)
+    
+    output = stitch_images_divide_conquer(corrected)
     if output is not None:
         visualize_output(frames, output)
+    
+    
+    # image_stitcher = cv2.Stitcher_create()
+    # err, output = image_stitcher.stitch(frames)
+    # if not err:
+    #     visualize_output(frames, output)
+    
+    return 
         
 if __name__ == "__main__":
     main()
